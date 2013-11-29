3626074d18eaab93eaefad82cd767267ba15a198
Ivan Paner <ivan.paner@gmail.com>
2013-11-30 04:33:35 +0800

    Offloaded category counting and text cleanup to Category

--- Flood Tweet Classifier/src/miner/Category.java
+++ Flood Tweet Classifier/src/miner/Category.java
@@ -21,6 +21,9 @@ public enum Category {
         else if (text.contains("#safenow")) {
             result = SAFENOW;
         }
+        else if (text.contains("#floodph")) {
+            result = FLOOD;
+        }
         else if (text.contains("#tracingph")) {
             result = TRACING;
         }
@@ -31,12 +34,48 @@ public enum Category {
         return result;
     }
     
+    public static String cleanUp(String text) {
+        String result = text.toLowerCase();
+        
+        text.replace("#reliefph", "");
+        text.replace("#rescueph", "");
+        text.replace("#safenow", "");
+        text.replace("#floodph", "");
+        text.replace("#tracingph", "");
+        text.replace("#yolandaph", "");
+        
+        return result;
+    }
+    
+    
+    
+    public static int countCategories(String text) {
+        int result = 0;
+        text = text.toLowerCase();
+        
+        if (text.contains("#reliefph")) {
+            result++;
+        }
+        if (text.contains("#rescueph")) {
+            result++;
+        }
+        if (text.contains("#safenow")) {
+            result++;
+        }
+        if (text.contains("#tracingph")) {
+            result++;
+        }
+        if (text.contains("#yolandaph")) {
+            result++;
+        }
+        
+        return result;
+    }
     
     String name;
     Category(String name) {
         this.name = name;
     }
-    
     
     @Override
     public String toString() {
